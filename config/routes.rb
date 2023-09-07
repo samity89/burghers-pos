ef9b1d636252eb8785c8a7f664a1776c533ef748
Samuel Amity <sam.amity@gmail.com>
2023-09-07 15:35:59 -0400

    deploy script fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :orders, :checks, :products
   resources :users
   devise_for :users
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
