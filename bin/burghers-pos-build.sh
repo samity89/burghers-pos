dc89f9e3402d56d22b7a72c043a2ed2dc3a8da15
Samuel Amity <sam.amity@gmail.com>
2023-09-07 15:22:24 -0400

    deployment script fix

--- bin/burghers-pos-build.sh
+++ bin/burghers-pos-build.sh
@@ -3,8 +3,8 @@ set -o errexit
 
 # Add build commands for front end
 rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
+npm install --prefix frontend && npm run build --prefix frontend
+cp -a frontend/build/. public/
 
 bundle install
 bundle exec rake db:migrate
