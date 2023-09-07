8f15888a378c15895a48a55395dcabf7c761b4f4
Samuel Amity <sam.amity@gmail.com>
2023-09-07 15:06:18 -0400

    readying for deployment

---
+++ bin/burghers-pos-build.sh
@@ -0,0 +1,10 @@
+# exit on error
+set -o errexit
+
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+bundle install
+bundle exec rake db:migrate
